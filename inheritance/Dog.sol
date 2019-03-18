8970fee61c1837f447b134415a97f5265bb01968
gabriel.kuma <gab.kuma@gmail.com>
2019-03-18 02:14:04 +0100

    Make Dog contract payable, add getBalance

--- inheritance/Dog.sol
+++ inheritance/Dog.sol
@@ -3,10 +3,21 @@ pragma solidity 0.5.1;
 import "./Animal.sol";
 
 contract DogContract is AnimalContract{
+    
+    modifier cost(uint _amount){
+        require(msg.value >= _amount);
+        _;
+        if(msg.value > _amount){
+            require(msg.sender.send(msg.value - _amount) == true, "transfert function failed");
+        }
+    }
 
-    function addDog(string memory _name, uint _age) public onlyOwner returns(uint){
+    function addDog(string memory _name, uint _age) public payable cost(1000) onlyOwner returns(uint){
         return _addAnimal(_name, _age, AnimalType.DOG);
         
     }
 
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
 }
